6f9bcb89721c605e11bdf3aec7f27e7d92a31ccd
Mitra <mairmeds@gmail.com>
2018-04-16 22:23:55 -0400

    update version1

--- src/main/java/com/example/demo/Person.java
+++ src/main/java/com/example/demo/Person.java
@@ -1,23 +1,32 @@
 package com.example.demo;
 
+import org.hibernate.validator.constraints.Range;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
-
+    @NotNull
+    @Size(min=2)
     private String firstName;
-
+    @NotNull
+    @Size(min=2)
     private String lastName;
-
+    @NotNull
+    @Size(min=5)
     private String address;
-
+    @NotNull
+    @Size(min=10)
     private String phoneNumber;
+
 
     public long getId() {
         return id;
